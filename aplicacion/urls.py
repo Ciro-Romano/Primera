7705562d5492b582bc8ef0cb5161195cd115986a
Ciro <romano.agustin24@gmial.com>
2024-11-22 21:19:08 -0300

    Agregamos botones y funciones

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from aplicacion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Inicio, name='Inicio'),
+    path('Gato/', views.Gato, name='Gato')
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def Inicio(request):
     return render(request, 'aplicacion/inicio.html')
+
+def Gato(request):
+    return render(request, 'aplicacion/gato.html')
